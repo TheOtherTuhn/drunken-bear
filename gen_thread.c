86973187a489a5e4e34330c6f0cbc6446682ff9e
Thure Leiste <tleiste96@gmail.com>
2015-03-09 18:01:59 +0100

    added gen_thread.{c,,h}

---
+++ gen_thread.c
@@ -0,0 +1,152 @@
+#include "gen_thread.h"
+
+struct {
+    pthread_mutex_t m;
+    int count;
+    ThreadCommand command;
+} thread_info = {PTHREAD_MUTEX_INITIALIZER, 0};
+
+void set_thread_command(ThreadCommand tc)
+{
+    thread_info.command = tc;
+}
+
+int get_thread_count(void)
+{
+    return thread_info.count;
+}
+
+void increase_thread_count(void)
+{
+    pthread_mutex_lock(&thread_info.m);
+        thread_info.count++;
+    pthread_mutex_unlock(&thread_info.m);
+}
+
+void decrease_thread_count(void)
+{
+    pthread_mutex_lock(&thread_info.m);
+        thread_info.count--;
+    pthread_mutex_unlock(&thread_info.m);
+}
+
+/*c function to generate moves*/
+int generate(game_state *old)
+{
+    int i, j;
+    int count = 0;
+    game_state *prv, *dummy;
+    if(old->turn < 7) {
+        for(i = 0; i < 64; i++) {
+            if(old->fields[i].rpen == 0 && 0 == old->fields[i].bpen \
+            && old->fields[i].fish == 1) {
+                if((old->r_current && old->leftR > 0) \
+                ||(old->b_current && old->leftB > 0)) {
+                    dummy = simulate_set_move(old, prv, i);
+                    qpush(dummy);
+                    count++;
+                    prv = dummy;
+                }
+            }
+        }
+    } else {
+        prv = spawn_gs(old);
+        prv->last_move = (move) {Null, 0, 0};
+        old->first = prv;
+        qpush(prv);
+        for(i = 0; i < 64; i++) {
+            if((old->fields[i].rpen && old->b_current) \
+            || (old->fields[i].bpen && old->r_current)) {
+                for(j = E(i); INBOUNDS(j) \
+                    && old->fields[j].fish \
+                    && old->fields[j].bpen == old->fields[j].rpen; j = E(j)) {
+                    dummy = simulate_run_move(old, prv, i, j);
+                    qpush(dummy);
+                    count++;
+                    prv = dummy;
+                }
+                for(j = NE(i); INBOUNDS(j) \
+                    && old->fields[j].fish \
+                    && old->fields[j].bpen == old->fields[j].rpen; j = NE(j)) {
+                    dummy = simulate_run_move(old, prv, i, j);
+                    qpush(dummy);
+                    count++;
+                    prv = dummy;
+                }
+                for(j = NW(i); INBOUNDS(j) \
+                    && old->fields[j].fish \
+                    && old->fields[j].bpen == old->fields[j].rpen; j = NW(j)) {
+                    dummy = simulate_run_move(old, prv, i, j);
+                    qpush(dummy);
+                    count++;
+                    prv = dummy;
+                }
+                for(j = W(i); INBOUNDS(j) \
+                    && old->fields[j].fish \
+                    && old->fields[j].bpen == old->fields[j].rpen; j = W(j)) {
+                    dummy = simulate_run_move(old, prv, i, j);
+                    qpush(dummy);
+                    count++;
+                    prv = dummy;
+                }
+                for(j = SW(i); INBOUNDS(j) \
+                    && old->fields[j].fish \
+                    && old->fields[j].bpen == old->fields[j].rpen; j = SW(j)) {
+                    dummy = simulate_run_move(old, prv, i, j);
+                    qpush(dummy);
+                    count++;
+                    prv = dummy;
+                }
+                for(j = SE(i); INBOUNDS(j) \
+                    && old->fields[j].fish \
+                    && old->fields[j].bpen == old->fields[j].rpen; j = SE(j)) {
+                    dummy = simulate_run_move(old, prv, i, j);
+                    qpush(dummy);
+                    count++;
+                    prv = dummy;
+                }
+            }
+        }
+    }
+    return count;
+}
+
+void *gen_gs(void *args)
+{
+    int running = 1;
+    game_state *old;
+    int *gs_count = malloc(sizeof(int));
+    *gs_count = 0;
+    increase_thread_count();
+    struct{game_state*gs;int war; char sid[37];int turn;} *current_gs = args;
+    while(1) {
+        switch(thread_info.command) {
+        case Generate:
+            if(!running) {
+                DBUG("I generated %d moves!\n", *gs_count);
+                *gs_count = 0;
+                increase_thread_count();
+                running = 1;
+            }
+            if(!(old = qpop()) || old->turn > current_gs->gs->turn)
+                usleep(200);
+            else
+                *gs_count += generate(old);
+            break;
+        case Exit:
+            if(running)
+                decrease_thread_count();
+            pthread_exit(gs_count);
+            break;
+        default:
+        case Wait:
+            if(running) {
+                decrease_thread_count();
+                running = 0;
+            }
+            usleep(200);
+            break;
+        }
+    }
+    return gs_count;
+}
---
+++ gen_thread.h
@@ -0,0 +1,41 @@
+#ifndef __GEN_THREAD_H__
+#define __GEN_THREAD_H__
+
+#include <pthread.h>
+#include <stdio.h>
+#include <unistd.h>
+
+#include "gs.h"
+#include "q.h"
+
+#define DEBUG 1
+#ifdef DEBUG
+# if DEBUG == 1
+#  define DBUG(s, ...) fprintf(stderr, s, ##__VA_ARGS__)
+# elif DEBUG == 2
+#  define DBUG(s, ...) fprintf(stderr, "%3d", __LINE__); fprintf(stderr, s, ##__VA_ARGS__)
+# else
+#  define DBUG(s, ...) fprintf(stderr, "<%s>:%3d: ", __FILE__, __LINE__); fprintf(stderr, s, ##__VA_ARGS)
+# endif
+#else
+# define DBUG(s, ...)
+#endif
+
+#define E(f)  (f+8)
+#define NE(f) (f+(f&1)?-1:7)
+#define NW(f) (f+(f&1)?-9:-1)
+#define W(f)  (f-8)
+#define SW(f) (f+(f&1)?-7:1)
+#define SE(f) (f+(f&1)?1:9)
+#define INBOUNDS(f) ((0<=f)&&(f<64))
+
+typedef enum {Generate, Wait, Exit} ThreadCommand;
+
+void set_thread_command(ThreadCommand);
+int get_thread_count(void);
+void increase_thread_count(void);
+void decrease_thread_count(void);
+int generate(game_state*);
+void *gen_gs(void*);
+
+#endif
